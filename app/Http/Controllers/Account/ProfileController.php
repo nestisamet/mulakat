910823f511f4dbc9180a38207e741466bb81c87c
Samet <nestisamet@gmail.com>
2019-09-24 14:17:27 +0300

    http > account > profile > update

--- app/Http/Controllers/Account/ProfileController.php
+++ app/Http/Controllers/Account/ProfileController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Account;
 
+use App\Exceptions\ApiException;
 use App\Http\Controllers\Controller;
 use App\Http\Validation\RequestDataValidation;
 use App\Repository\Contracts\AccountRepository;
 use Illuminate\Http\Request;
-use Validator;
 use App\Http\Resources\Account as AccountResource;
 
 class ProfileController extends Controller
@@ -34,9 +34,28 @@ class ProfileController extends Controller
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
+     * @throws \App\Exceptions\ApiException
      */
     protected function store(Request $request)
     {
+        $this->rules = [
+            'name' => ['required', 'string', 'max:20'],
+            'surname' => ['required', 'string', 'max:50']
+        ];
 
+        $data = $request->only(array_keys($this->rules));
+        try {
+            $this->validateRequest($data);
+            $this->storage->update(auth()->user()->id, $data);
+            return response()->json([
+                'success' => true,
+                'message' => [
+                    'general' => trans('account.profile.updated')
+                ]
+            ]);
+        }
+        catch (ApiException $e) {
+            return response()->json($e->getMsg(), $e->getResponseCode());
+        }
     }
 }
--- resources/lang/en/account.php
+++ resources/lang/en/account.php
@@ -4,5 +4,9 @@ return [
     'signup' => [
         'notExistsCode' => 'Your code not match any student',
         'success' => 'You have registered successfully'
+    ],
+
+    'profile' => [
+        'updated' => 'Profile info updated successfully'
     ]
 ];
